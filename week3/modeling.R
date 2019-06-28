7d9277719b57bdd2867c7ececca82e71174a3529
Etta Rapp <ettirapp@gmail.com>
2019-06-28 14:32:10 -0400

    going through the notebooks...

--- week3/modeling.R
+++ week3/modeling.R
@@ -19,20 +19,16 @@ ggplot(users, aes(x = daily.views)) +
   scale_y_continuous(label = comma) +
   xlab('Daily pageviews') +
   ylab('')
-ggsave(filename='figures/daily_pageviews_dist.pdf', width=4, height=4)
-
 
 ggplot(data = users, aes(x = age, y = daily.views)) +
   geom_point() +
   facet_wrap(~ gender) +
   xlab('Age') +
   ylab('Daily pageviews')
-ggsave(filename='figures/daily_pageviews_by_age_and_gender.pdf', width=8, height=4)
 
 nrow(users)
 users <- filter(users, daily.views > 0)
 nrow(users)
-
 
 views_by_age_and_gender <- users %>%
   filter(age <= 90) %>%
@@ -50,8 +46,6 @@ ggplot(views_by_age_and_gender, aes(x = age, y = median_daily_views, color = gen
   ylab('Daily pageviews') +
   scale_size_area(guide = F) +
   theme(legend.title=element_blank())
-ggsave(filename='figures/median_daily_pageviews_by_age_and_gender.pdf', width=8, height=4)
-
 
 model_data <- filter(users, age >= 18 & age <= 65)
 
@@ -168,4 +162,129 @@ ggplot(plot_data, aes(x = age, y = pred, color = gender)) +
   geom_line(aes(y = pred)) +
   geom_point(aes(y = geom_mean_daily_views, size = count)) +
   scale_size_area(guide = F)
-ggsave(filename='figures/modeled_daily_pageviews_by_age_and_gender.pdf', width=8, height=4)
+
+#EVALUATING MODELS:
+
+library(tidyverse)
+library(scales)
+
+library(modelr)
+options(na.action = na.warn)
+
+library(broom)
+
+theme_set(theme_bw())
+options(repr.plot.width=4, repr.plot.height=3)
+
+users <- read_tsv(gzfile('users.tsv.gz'))
+
+model_data <- filter(users, daily.views > 0, age >= 18 & age <= 65)
+
+form <- as.formula(log10(daily.views) ~ gender * (age + I(age^2)))
+M <- model.matrix(form, model_data)
+head(M)
+
+
+model <- lm(form, model_data)
+tidy(model)
+glance(model)
+
+
+plot_data <- model_data %>%
+  group_by(age, gender) %>%
+  summarize(count = n(),
+            geom_mean_daily_views = 10^(mean(log10(daily.views)))) %>%
+  add_predictions(model) %>%
+  mutate(pred = 10^pred)
+
+options(repr.plot.width=6, repr.plot.height=3)
+ggplot(plot_data, aes(x = age, y = pred, color = gender)) +
+  geom_line(aes(y = pred)) +
+  geom_point(aes(y = geom_mean_daily_views, size = count)) +
+  scale_size_area(guide = F)
+
+
+options(repr.plot.width=4, repr.plot.height=3)
+
+ggplot(plot_data, aes(x = pred, y = geom_mean_daily_views)) +
+  geom_point() +
+  geom_abline(linetype = "dashed") +
+  xlab('Predicted') +
+  ylab('Actual')
+
+
+ggplot(plot_data, aes(x = pred, y = geom_mean_daily_views, color = gender)) +
+  geom_point() +
+  geom_abline(linetype = "dashed") +
+  xlab('Predicted') +
+  ylab('Actual')
+
+
+ggplot(plot_data, aes(x = pred, y = geom_mean_daily_views, color = desc(age))) +
+  geom_point() +
+  geom_abline(linetype = "dashed") +
+  xlab('Predicted') +
+  ylab('Actual') +
+  facet_wrap(~ gender, scale = "free")
+
+pred_actual <- model_data %>%
+  add_predictions(model) %>%
+  mutate(actual = log10(daily.views))
+
+head(pred_actual)
+
+ggplot(pred_actual, aes(x = 10^pred, y = 10^actual)) +
+  geom_point(alpha = 0.1) +
+  geom_abline(linetype = "dashed") +
+  scale_x_log10(label = comma, breaks = seq(0,100,by=10)) +
+  scale_y_log10(label = comma) +
+  xlab('Predicted') +
+  ylab('Actual')
+
+
+pred_actual %>%
+  summarize(rmse = sqrt(mean((pred - actual)^2)))
+
+
+pred_actual %>%
+  summarize(rmse = sqrt(mean((10^pred - 10^actual)^2)))
+
+
+pred_actual %>%
+  summarize(rmse = sqrt(mean((pred - actual)^2)),
+            cor = cor(pred, actual),
+            cor_sq = cor^2)
+
+rmse(model, model_data)
+rsquare(model, model_data)
+
+pred_actual %>%
+  summarize(mse = mean((pred - actual)^2),
+            mse_baseline = mean((mean(actual) - actual)^2),
+            mse_vs_baseline = (mse_baseline - mse) / mse_baseline,
+            cor = cor(pred, actual),
+            cor_sq = cor^2)
+
+
+K <- 30
+form <- as.formula(log10(daily.views) ~ gender * poly(age, K, raw=T))
+M <- model.matrix(form, model_data)
+head(M)
+
+model <- lm(form, model_data)
+glance(model)
+tidy(model)
+
+
+plot_data <- model_data %>%
+  group_by(age, gender) %>%
+  summarize(count = n(),
+            geom_mean_daily_views = 10^(mean(log10(daily.views)))) %>%
+  add_predictions(model) %>%
+  mutate(pred = 10^pred)
+
+options(repr.plot.width=6, repr.plot.height=3)
+ggplot(plot_data, aes(x = age, y = pred, color = gender)) +
+  geom_line(aes(y = pred)) +
+  geom_point(aes(y = geom_mean_daily_views, size = count)) +
+  scale_size_area(guide = F)
